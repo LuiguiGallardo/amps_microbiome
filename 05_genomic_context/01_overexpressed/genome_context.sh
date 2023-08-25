3645b6d58b6e9ca66e516d059291cf6994594dce
LuiguiGallardo <luiguimichelgallardo@gmail.com>
2023-08-25 12:28:35 -0700

    Update to overexpressed AMPs genomic context

---
+++ 05_genomic_context/01_overexpressed/genome_context.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+gt sketch -flattenfiles yes 02_graphs/AMP_145418_c1_g1_i2.png 01_gff3/TRINITY_DN145418_c1_g1_i2.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_138200_c2_g1_i1.png 01_gff3/TRINITY_DN138200_c2_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_144897_c3_g1_i5.png 01_gff3/TRINITY_DN144897_c3_g1_i5.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_142228_c6_g1_i1.png 01_gff3/TRINITY_DN142228_c6_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_145547_c6_g1_i1.png 01_gff3/TRINITY_DN145547_c6_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_143096_c2_g1_i1.png 01_gff3/TRINITY_DN143096_c2_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_140597_c1_g8_i2.png 01_gff3/TRINITY_DN140597_c1_g8_i2.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_144119_c3_g1_i2.png 01_gff3/TRINITY_DN144119_c3_g1_i2.GFF3
+# gt sketch -flattenfiles yes 02_graphs/AMP_142823_c1_g1_i2.png 01_gff3/TRINITY_DN142823_c1_g1_i2.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_138484_c2_g1_i1.png 01_gff3/TRINITY_DN138484_c2_g1_i1.GFF3
+# gt sketch -flattenfiles yes 02_graphs/AMP_143018_c49_g12_i1.png 01_gff3/TRINITY_DN143018_c49_g12_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_143863_c3_g1_i1.png 01_gff3/TRINITY_DN143863_c3_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_142424_c1_g1_i2.png 01_gff3/TRINITY_DN142424_c1_g1_i2.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_144483_c7_g1_i1.png 01_gff3/TRINITY_DN144483_c7_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_141567_c4_g1_i3.png 01_gff3/TRINITY_DN141567_c4_g1_i3.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_615414_c0_g1_i1.png 01_gff3/TRINITY_DN615414_c0_g1_i1.GFF3
+# gt sketch -flattenfiles yes 02_graphs/AMP_851511_c0_g1_i1.png 01_gff3/TRINITY_DN851511_c0_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_141904_c4_g1_i1.png 01_gff3/TRINITY_DN141904_c4_g1_i1.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_145391_c5_g1_i9.png 01_gff3/TRINITY_DN145391_c5_g1_i9.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_145055_c3_g1_i3.png 01_gff3/TRINITY_DN145055_c3_g1_i3.GFF3
