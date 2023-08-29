9d9353bca99dabac285cc1259130530718a41099
LuiguiGallardo <luiguimichelgallardo@gmail.com>
2023-08-29 11:47:31 -0700

    Genomic context from AMPs present on all samples and top30 added

--- 05_genomic_context/02_allsamples/genome_context.sh
+++ 05_genomic_context/02_allsamples/genome_context.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-gt sketch -flattenfiles yes 02_graphs/AMP_145418_c1_g1_i2.png 01_gff3/TRINITY_DN145418_c1_g1_i2.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_138200_c2_g1_i1.png 01_gff3/TRINITY_DN138200_c2_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_144897_c3_g1_i5.png 01_gff3/TRINITY_DN144897_c3_g1_i5.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_142228_c6_g1_i1.png 01_gff3/TRINITY_DN142228_c6_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_145547_c6_g1_i1.png 01_gff3/TRINITY_DN145547_c6_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_143096_c2_g1_i1.png 01_gff3/TRINITY_DN143096_c2_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_140597_c1_g8_i2.png 01_gff3/TRINITY_DN140597_c1_g8_i2.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_144119_c3_g1_i2.png 01_gff3/TRINITY_DN144119_c3_g1_i2.GFF3
-# gt sketch -flattenfiles yes 02_graphs/AMP_142823_c1_g1_i2.png 01_gff3/TRINITY_DN142823_c1_g1_i2.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_138484_c2_g1_i1.png 01_gff3/TRINITY_DN138484_c2_g1_i1.GFF3
-# gt sketch -flattenfiles yes 02_graphs/AMP_143018_c49_g12_i1.png 01_gff3/TRINITY_DN143018_c49_g12_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_143863_c3_g1_i1.png 01_gff3/TRINITY_DN143863_c3_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_142424_c1_g1_i2.png 01_gff3/TRINITY_DN142424_c1_g1_i2.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_144483_c7_g1_i1.png 01_gff3/TRINITY_DN144483_c7_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_141567_c4_g1_i3.png 01_gff3/TRINITY_DN141567_c4_g1_i3.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_615414_c0_g1_i1.png 01_gff3/TRINITY_DN615414_c0_g1_i1.GFF3
-# gt sketch -flattenfiles yes 02_graphs/AMP_851511_c0_g1_i1.png 01_gff3/TRINITY_DN851511_c0_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_141904_c4_g1_i1.png 01_gff3/TRINITY_DN141904_c4_g1_i1.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_145391_c5_g1_i9.png 01_gff3/TRINITY_DN145391_c5_g1_i9.GFF3
-gt sketch -flattenfiles yes 02_graphs/AMP_145055_c3_g1_i3.png 01_gff3/TRINITY_DN145055_c3_g1_i3.GFF3
+gt sketch -flattenfiles yes 02_graphs/AMP_145857_c2_g1_i5.png 01_gff3/TRINITY_DN145857_c2_g1_i5.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_134571_c44_g1_i3.png 01_gff3/TRINITY_DN134571_c44_g1_i3.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_143867_c81_g1_i4.png 01_gff3/TRINITY_DN143867_c81_g1_i4.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_145422_c2_g1_i16.png 01_gff3/TRINITY_DN145422_c2_g1_i16.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_144440_c1_g1_i2.png 01_gff3/TRINITY_DN144440_c1_g1_i2.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_383713_c2_g1_i1.png 01_gff3/TRINITY_DN383713_c2_g1_i1.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_144616_c7_g3_i6.png 01_gff3/TRINITY_DN144616_c7_g3_i6.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_144746_c5_g1_i1.png 01_gff3/TRINITY_DN144746_c5_g1_i1.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_143402_c2_g1_i6.png 01_gff3/TRINITY_DN143402_c2_g1_i6.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_144878_c1_g1_i1.png 01_gff3/TRINITY_DN144878_c1_g1_i1.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_141259_c3_g4_i3.png 01_gff3/TRINITY_DN141259_c3_g4_i3.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_145462_c0_g1_i9.png 01_gff3/TRINITY_DN145462_c0_g1_i9.gff3
+gt sketch -flattenfiles yes 02_graphs/AMP_145289_c1_g1_i1.png 01_gff3/TRINITY_DN145289_c1_g1_i1.gff3
