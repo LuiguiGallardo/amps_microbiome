10bfcc546969abdc87180d06a88c203b974ad6ee
LuiguiGallardo <bfllg77@gmail.com>
2023-09-11 11:17:07 -0700

    Update to genomic context info

--- 05_genomic_context/02_allsamples/genome_context.sh
+++ 05_genomic_context/02_allsamples/genome_context.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-gt sketch -flattenfiles yes 02_graphs/AMP_145857_c2_g1_i5.png 01_gff3/TRINITY_DN145857_c2_g1_i5.gff3
 gt sketch -flattenfiles yes 02_graphs/AMP_134571_c44_g1_i3.png 01_gff3/TRINITY_DN134571_c44_g1_i3.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_143867_c81_g1_i4.png 01_gff3/TRINITY_DN143867_c81_g1_i4.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_145422_c2_g1_i16.png 01_gff3/TRINITY_DN145422_c2_g1_i16.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_144440_c1_g1_i2.png 01_gff3/TRINITY_DN144440_c1_g1_i2.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_383713_c2_g1_i1.png 01_gff3/TRINITY_DN383713_c2_g1_i1.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_144616_c7_g3_i6.png 01_gff3/TRINITY_DN144616_c7_g3_i6.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_144746_c5_g1_i1.png 01_gff3/TRINITY_DN144746_c5_g1_i1.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_143402_c2_g1_i6.png 01_gff3/TRINITY_DN143402_c2_g1_i6.gff3
 gt sketch -flattenfiles yes 02_graphs/AMP_144878_c1_g1_i1.png 01_gff3/TRINITY_DN144878_c1_g1_i1.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_141259_c3_g4_i3.png 01_gff3/TRINITY_DN141259_c3_g4_i3.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_145462_c0_g1_i9.png 01_gff3/TRINITY_DN145462_c0_g1_i9.gff3
-gt sketch -flattenfiles yes 02_graphs/AMP_145289_c1_g1_i1.png 01_gff3/TRINITY_DN145289_c1_g1_i1.gff3
